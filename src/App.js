9bec71922e6155f9599a0cb0ebba5d4d9908dc1d
filipenevola <filipe@quave.com.br>
2022-12-30 23:02:55 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+const API_RESPONSE = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={({ target: { value } }) => onFilterTextChange(value)}
+      />
+      <label>
+        <input
+          type="checkbox"
+          value={inStockOnly}
+          onChange={({ target: { checked } }) => onInStockOnlyChange(checked)}
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
+  );
+};
+
+const ProductCategoryRow = ({ category }) => (
+  <tr style={{ fontWeight: "bold" }}>
+    <td colSpan={2}>{category}</td>
+  </tr>
+);
+
+const ProductRow = ({ product }) => (
+  <tr style={{ color: product.stocked ? "" : "red" }}>
+    <td>{product.name}</td>
+    <td>{product.price}</td>
+  </tr>
+);
+
+const ProductTable = ({ products, filterText, inStockOnly }) => {
+  const [sort, setSort] = useState('');
+
+  const rows = products
+    .filter((product) => {
+      if (
+        filterText &&
+        !product.name.toLowerCase().includes(filterText.toLowerCase())
+      ) {
+        return false;
+      }
+      if (inStockOnly && !product.stocked) {
+        return false;
+      }
+      return true;
+    })
+    .sort((a, b) => {
+      if (!sort) {
+        return 0;
+      }
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return sort === 'asc' ? -1 : 1;
+      }
+      if (nameA > nameB) {
+        return sort === 'asc' ? 1 : -1;
+      }
+      return 0;
+    })
+    .reduce((acc, product, index, originalProducts) => {
+      const categoryRow = (
+        <ProductCategoryRow
+          key={product.category}
+          category={product.category}
+        />
+      );
+      const row = <ProductRow key={product.name} product={product} />;
+      const newCategory =
+        !sort && (index === 0 || product.category !== originalProducts[index - 1].category);
+      if (newCategory) {
+        return [...acc, categoryRow, row];
+      }
+
+      return [...acc, row];
+    }, []);
+
+  function onSort() {
+    setSort(!sort ? 'asc' : (sort === 'asc' ? 'desc' : ''))
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th onClick={onSort}>Name {sort ? `(${sort})` : ''}</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+/*
+FilterableProductTable
+SearchBar
+ProductTable
+ProductCategoryRow
+ProductRow
+ */
+const FilterableProductTable = ({ products }) => {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FilterableProductTable products={API_RESPONSE} />
     </div>
   );
 }
